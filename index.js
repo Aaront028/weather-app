022bed7167370b9e4100cb842843c6c202ed747c
xlwv <pkomaldeep@gmail.com>
2024-01-06 23:27:38 +0530

    ready to style

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// import { JSDOM } from 'jsdom';
-// import jQuery from 'jquery';
-// const { window } = new JSDOM('');
-// const $ = jQuery(window);
+
 import express from 'express'
 import axios from 'axios'
 import { dirname, join } from 'path'
@@ -11,32 +8,23 @@ const app = express()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(join(__dirname)))
 
-
-
-
-
 const port = 3000;
-//window.jQuery = window.$ = $; 
+
 const config = {
  
-  headers: {
-    key:"69bec42602954187bf8125713240301"
-
-
-  }
-};
-//app.use(bodyParser.json());
-
-//app.use(bodyParser.urlencoded({ extended: true }));
+  headers: 
+  {
+    key:"69bec42602954187bf8125713240301",
+  },
+}
 
 console.log("inde.js working");
 app.get("/",(req,res)=>{
-res.sendFile(__dirname,"/index.html");
+res.sendFile(join(__dirname, '/index.html'));
 });
 app.post("/post",async(req,res)=>{
 try
@@ -44,24 +32,21 @@ try
 const place=req.body.location;
 console.log(place);
 const result=await axios.get(`http://api.weatherapi.com/v1/current.json?key=${config.headers.key}&q=${place}`);
-console.log(result.data);
-//res.send(JSON.stringify(result.data,null,2).replace(/(?:\r\n|\r|\n)/g, '<br>'));
+console.log(result);
 res.json(result.data);
 
 }
 catch(error)
 {
-  //console.error(error);
+  console.error(error.response || error)
+  res.status(500).send('Internal Server Error')
 }
+
 });
 
 
-
-
-
-
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port  http://localhost:${port}`);
 });
 
 
--- main.js
+++ main.js
@@ -1,82 +1,30 @@
-$(document).ready(function () {
-    $('#formid').submit(function (event) {
-      event.preventDefault()
-      const location = $('#location').val()
-  
-      $.ajax({
-        type: 'POST',
-        url: '/post',
-        data: { location: location },
-        success: function (response) {
-          console.log(response)
-          $('#result').html(`
-          <p>Name: ${response.location.name}</p>
-          <p>Temperature: ${response.current.temp_c}</p>
-          <p>Humidity: ${response.current.humidity}</p>
-          <label>${response.current.condition.text}</label>
-          <img src="${response.current.condition.icon}"/>
-          `)
-        },
-        error: function (error) {
-          console.error('Error fetching data:', error)
-        },
-      })
-    })
-  })
+
+const form=$("#formid");
+const loc=$("#location");
+form.submit(async function(event){
+try
+{
+    event.preventDefault();
+    const location = loc.val();
+    const response=await axios.post("/post",{location:location});
+    console.log(response.data);
+    
+    $("#result").html(`
+    <p>Temperature: ${response.data.current.temp_c}</p>
+    <p>Humidity: ${response.data.current.humidity}</p>
+    <p>${response.data.current.condition.text}</p>
+    <img src="${response.data.current.condition.icon}"/>
+    `)
+    
+    
+}    
+
+catch(error)
+{
+    console.error('error:',error);
+}
+
+});
 
 
 
-
-
-
-
-
-
-
-
-
-
-            
-// import { JSDOM } from 'jsdom';
-// import axios from 'axios';
-// import jQuery from 'jquery';
-// const { window } = new JSDOM();
-// const $ = jQuery(window);
-
-
-
-
-// const form=$("#formid");
-// const loc=$("#location");
-// form.submit(async function(event){
-
-
-// try
-// {
-//     const location = loc.val();
-//     const response=await axios.post("/post",{location:location});
-//     console.log(response.data);
-//     $("#result").html(response.current.temp_c);
-// }    
-// catch(error)
-// {
-//     console.error('error:',error);
-// }
-
-// });
-
-// // function showdata(data)
-// // {
-// //     const result =$("#result");
-// //     result.html(data);
-// //     console.log('Data displayed successfully:', data);
-
-// // }
-
-
-// console.log("check main.js");
-// export function greet(name) {
-//     return `Hello, ${name}`;
-//   }
-  
-//   export const message = "How you doing?";
